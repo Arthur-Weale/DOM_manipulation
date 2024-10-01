2d6b2f769fdb5a9bc75a9971b055216180002a9f
Arthur Weale <ajayrossy340@gmail.com>
2024-10-01 02:02:18 +0200

    All is done

---
+++ functions_with_javascript/sandbox.js
@@ -0,0 +1,24 @@
+
+
+    let a = 7
+    let b = 3
+function add7(){
+    return a + b;
+}
+
+console.log(add7())
+
+function multiply(a,b){
+    return a * b
+}
+
+console.log(multiply(3,4))
+
+
+    let x = 'arthur'
+    const y = x.slice(1);
+function captilize(){
+    return x.charAt(0).toUpperCase() + y ;
+}
+
+console.log(captilize())
